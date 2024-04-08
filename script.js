59db29708a1f76a1b360acb9670d480c85922326
Kevyn Mendes <kevyn.movie@gmail.com>
2024-04-08 20:12:04 -0300

    Update.3.0

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function changeChannel(event) {
     var iframe = document.getElementById("player");
     iframe.src = link;
 }
+
+window.onload = function() {
+    var today = new Date();
+    var dayOfWeek = today.getDay();
+
+    var imageSrc;
+    var linkBackgroundColor;
+    
+    if (dayOfWeek === 0 || dayOfWeek === 1 || dayOfWeek === 2) { // Domingo (0), Segunda (1) ou Terça (2)
+        imageSrc = "src/raw.jpg";
+        linkBackgroundColor = "crimson"; 
+    } else if (dayOfWeek >= 3 && dayOfWeek <= 5) { // Quarta (3) a Sexta (5)
+        imageSrc = "src/smackdown.jpg";
+        linkBackgroundColor = "blue"; 
+    } else { // Sábado (6)
+        imageSrc = "src/WM40.jpg";
+        linkBackgroundColor = "crimson";
+    }
+
+    var player = document.getElementById("player");
+    player.src = imageSrc;
+
+    var links = document.querySelectorAll("ul#canais li a");
+    links.forEach(function(link) {
+        link.style.backgroundColor = linkBackgroundColor;
+    });
+};
+
